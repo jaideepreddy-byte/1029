fe53d333960e56b7f6ec8b0a48109bb3660ef2e4
Jaideep Reddy <75984238+jaideepreddy-byte@users.noreply.github.com>
2023-05-12 23:48:35 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ import subprocess
 REMOTE_SERVER = "13.233.123.233"
 REMOTE_USER = "ec2-user"
 REMOTE_DIR = "/var/www/html/1029"
-LOCAL_DIR = "/var/lib/jenkins/workspace/1029"
+LOCAL_DIR = "/var/lib/jenkins/workspace/1029/index.html"
 
 # Create the target directory on the remote server if it doesn't exist
 ssh_command = f"ssh {REMOTE_USER}@{REMOTE_SERVER} 'mkdir -p {REMOTE_DIR}'"
