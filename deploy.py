ad37fdc266323251f90bde4072961fb075a9d4e4
Jaideep Reddy <75984238+jaideepreddy-byte@users.noreply.github.com>
2023-05-12 23:29:14 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,10 +2,10 @@ import os
 import subprocess
 
 # Set up the deployment variables
-REMOTE_SERVER = "your_remote_server_ip"
-REMOTE_USER = "your_remote_server_username"
-REMOTE_DIR = "/path/to/target/directory"
-LOCAL_DIR = "/path/to/cloned/repository"
+REMOTE_SERVER = "13.233.123.233"
+REMOTE_USER = "ec2-user"
+REMOTE_DIR = "/var/www/html/1029"
+LOCAL_DIR = "/var/lib/jenkins"
 
 # Create the target directory on the remote server if it doesn't exist
 ssh_command = f"ssh {REMOTE_USER}@{REMOTE_SERVER} 'mkdir -p {REMOTE_DIR}'"
